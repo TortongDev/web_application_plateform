14333df1d07254a15b2b7f068fe79de7c8d20427
kittithat <tortong9191@gmail.com>
2024-10-04 16:37:18 +0700

    change script

--- insert_transition.php
+++ insert_transition.php
@@ -2,8 +2,14 @@
 
 	require_once "./Connection.php";
 	$con = new connection();
-	$stmt = $con->db->prepare("insert into chache_data (data, data_desc, avatar) values(?, ?, ?)");
-	$stmt->execute(['','','']);
-	
+
+	$data1 = "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley";
+	$data2 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley";
+	$data3 = "picture-avatar.jpg";
+	for ($i = 0; $i <= 30000; $i++):
+		$stmt = $con->db->prepare("insert into chache_data (data, data_desc, avatar) values(?, ?, ?)");
+		$stmt->execute([$data1."=>".$i,$data2,$data3]);
+		$i++;
+	endfor;
 	exit;
 ?>
